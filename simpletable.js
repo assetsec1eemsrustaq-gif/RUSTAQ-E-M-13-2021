f5cd674b72b79db4309fb179a619d9b467c4e633
assetsec1eemsrustaq-gif <assetsec1eemsrustaq@gmail.com>
2025-11-16 14:34:53 +0400

    Fix filename casing for SimpleTable

---
+++ simpletable.js
@@ -0,0 +1,4 @@
+git mv src/components/SimpleTable.js src/components/SimpleTable_temp.js
+git mv src/components/SimpleTable_temp.js src/components/SimpleTable.js
+git commit -m "Fix SimpleTable filename casing"
+git push
